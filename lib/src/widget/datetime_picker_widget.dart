3ee17a9f7dba383214aadec4d4fe1a23def77cac
huoxiao5 <34760173+huoxiao5@users.noreply.github.com>
2021-09-29 11:20:16 +0800

    Update datetime_picker_widget.dart
    add selectionOverlay: widget.pickerTheme.selectionOverlay,  to CupertinoPicker.builder

--- lib/src/widget/datetime_picker_widget.dart
+++ lib/src/widget/datetime_picker_widget.dart
@@ -299,10 +299,11 @@ class _DateTimePickerWidgetState extends State<DateTimePickerWidget> {
       child: CupertinoPicker.builder(
         backgroundColor: widget.pickerTheme.backgroundColor,
         scrollController: scrollCtrl,
-        selectionOverlay: Container(
-            decoration: BoxDecoration(
-                border: Border.symmetric(
-                    horizontal: BorderSide(color: Colors.black26)))),
+        selectionOverlay: widget.pickerTheme.selectionOverlay,
+//         selectionOverlay: Container(
+//             decoration: BoxDecoration(
+//                 border: Border.symmetric(
+//                     horizontal: BorderSide(color: Colors.black26)))),
         itemExtent: widget.pickerTheme.itemHeight,
         onSelectedItemChanged: valueChanged,
         childCount: format.contains('m')
